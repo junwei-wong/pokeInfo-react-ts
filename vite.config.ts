cd74396572b426e149a8733097bce24c4bef0403
junwei-wong <weijwong@gmail.com>
2024-11-01 10:54:48 +0800

    Update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from "vitest/config"
 import react from "@vitejs/plugin-react"
-import { TanStackRouterVite } from "@tanstack/router-plugin/vite"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/pokeInfo-react-ts",
   server: {
     open: true,
   },
